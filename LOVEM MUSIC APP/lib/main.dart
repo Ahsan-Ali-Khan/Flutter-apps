6c09528f2f7d1febd5e3f78be3dbf2e003e0d689
Ahsan-Ali-Khan <61752339+Ahsan-Ali-Khan@users.noreply.github.com>
2020-12-28 12:14:28 +0530

    Update main.dart

--- LOVEM MUSIC APP/lib/main.dart
+++ LOVEM MUSIC APP/lib/main.dart
@@ -6,10 +6,6 @@ import 'package:lovem_a_music_app/screens/intro_screen.dart';
 void main() async{ //to use await we need to make it asynchronouss
   WidgetsFlutterBinding.ensureInitialized(); //ensures firebase is initialized
   await Firebase.initializeApp(); //to initialize firebase , since it returns future then we have to use await
-  // User user = new User();
-  // user.name="Ahsan";
-  // user.image="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png";
-  // user.email="abc@gmail.com";
   runApp(MaterialApp(
     title:'LOVEM',
     debugShowCheckedModeBanner: false,
